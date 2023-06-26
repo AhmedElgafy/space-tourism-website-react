c9b512764911ac87cd1b132d9c91de920100367a
unknown <AhmedElgafy>
2023-06-26 16:39:42 +0300

    first sprint

--- space-tourism-website-react/src/App.tsx
+++ space-tourism-website-react/src/App.tsx
@@ -1,10 +1,11 @@
 import { useState, createContext } from "react";
 import srcData from "../public/data.json";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./Routes/HomePage";
 import Destinations from "./Routes/Destinations";
 import Crew from "./Routes/Crew";
 import Technology from "./Routes/technology";
+import ToggleNav from "./ToggleNav";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,16 +13,23 @@ function App() {
 
   return (
     <>
+      {/* <Link to="/">home</Link> */}
+      {/* <ToggleNav imgSrc={srcData.nav[0].exitIcon} /> */}
       <nav className="m-6 flex items-center justify-between">
         <img className="block" src={srcData.nav[0].logoImage} alt="" />
         <img className="block" src={srcData.nav[0].hamburgerImage} alt="" />
       </nav>
       <BrowserRouter>
         <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/destinations" element={<Destinations />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
+          <Route
+            path="/"
+            element={<ToggleNav imgSrc={srcData.nav[0].exitIcon} />}
+          >
+            <Route index element={<HomePage />} />
+            <Route path="destinations" element={<Destinations />} />
+            <Route path="crew" element={<Crew />} />
+            <Route path="technology" element={<Technology />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
--- space-tourism-website-react/src/Routes/HomePage.tsx
+++ space-tourism-website-react/src/Routes/HomePage.tsx
@@ -1,16 +1,26 @@
 import scrData from "../../public/data.json";
 export default function HomePage() {
-  console.log(scrData.homePage[0].bgImageMob);
+  // console.log(scrData.homePage[0].bgImageMob);
   document.body.style.background = `url(${scrData.homePage[0].bgImageMob})  no-repeat `;
   return (
     <>
-      <div className="text-white flex flex-col h-full items-center text-center">
-        <h5 className="font-['Barlow Condensed']">
+      <div className="text-white flex flex-col h-screen justify-center text-center">
+        <h5 className="my-3 font-['Barlow Condensed', sans-serif] ">
           {scrData.homePage[0].subTitle}
         </h5>
-        <h1 className="font-['Bellefair']">{scrData.homePage[0].title}</h1>
-        <p className="font-['Barlow']">{scrData.homePage[0].description}</p>
-        <button className="font-['Bellefair']">EXPLORE</button>
+        <h1 className="my-3 font-['Bellefair'] font-light text-7xl">
+          {scrData.homePage[0].title}
+        </h1>
+        <p className="mb-10 mx-4 font-['Barlow', sans-serif] ">
+          {scrData.homePage[0].description}
+        </p>
+        <button
+          className="  font-['Bellefair'] mx-auto h-40 w-40  rounded-[100%] 
+
+text-[#0B0D17] text-2xl bg-[#ffffff] "
+        >
+          EXPLORE
+        </button>
       </div>
     </>
   );
